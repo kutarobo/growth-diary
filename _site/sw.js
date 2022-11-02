0bd8756e85ddfeee92b736b2d350c3b714f72673
kutarobo <kutarobo@naver.com>
2022-11-02 21:48:24 +0900

    bundle install

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/growth-diary/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221102.204019';
+const cacheName = 'chirpy-20221102.204749';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
